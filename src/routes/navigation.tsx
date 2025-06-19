f1da254f9275fb89a5bdbcba232cdcdfb14d708d
maryelinv <maryelinv>
2025-06-19 18:50:29 -0400

    feat: fixed active routes logic

--- src/routes/navigation.tsx
+++ src/routes/navigation.tsx
@@ -10,15 +10,15 @@ export const Navigation = () => {
           <ul>
             <li>
               <NavLink to="/home" className={
-                (isActive) => isActive ? 'nav-active' : ''}>Home</NavLink>
+                ({ isActive }) => isActive ? 'nav-active' : ''}>Home</NavLink>
             </li>
             <li>
               <NavLink to="/about" className={
-                (isActive) => isActive ? 'nav-active' : ''}>About</NavLink>
+                ({ isActive }) => isActive ? 'nav-active' : ''}>About</NavLink>
             </li>
             <li>
               <NavLink to="/users" className={
-                (isActive) => isActive ? 'nav-active' : ''}>Users</NavLink>
+                ({ isActive }) => isActive ? 'nav-active' : ''}>Users</NavLink>
             </li>
           </ul>
         </nav>
